26acb9b2d085af32ac709266ffeb341afb3e9366
prasaddk056 <95524653+prasaddk056@users.noreply.github.com>
2024-03-18 23:03:35 +0530

    Add files via upload

---
+++ 4.Captcha Generator/script.js
@@ -0,0 +1,51 @@
+const display = document.getElementById("_status");
+const body = document.body;
+const submit = document.getElementById("_submit");
+const refresh = document.getElementById("_refresh");
+
+const char = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let captcha = "";
+
+body.onload = function generateCaptcha(){
+
+    const captchaLength = 6;
+
+    for(let i = 0 ; i < captchaLength ; i++) {
+        const randomIndex = Math.floor(Math.random() * char.length);
+        captcha += char.substring(randomIndex , randomIndex + 1);
+    
+    }
+
+    document.getElementById("_generator").value = captcha;
+    display.innerText = "Captcha Generator";
+
+}
+
+submit.onclick = function checkInput() {
+    const input = document.getElementById("_clienttext").value;
+
+    if(input === ""){
+        display.innerText = "Please Enter the text shown below 👇";
+    } else if(input === captcha) {
+        display.innerText = "Matched 😎";
+    } else {
+        display.innerText = "Not-Matched 😑";
+    }
+
+}
+
+refresh.onclick = function refreshCaptcha() {
+
+    let newCaptcha = "";
+
+    for(let i = 0 ; i < captcha.length ; i++) {
+        const randomChar = char[Math.floor(Math.random() * char.length)];
+        newCaptcha += randomChar;
+    }
+
+    captcha = newCaptcha;
+
+    document.getElementById("_generator").value = captcha;
+    display.innerText = "Captcha Generator";
+}
