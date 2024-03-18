98aae30011a963449718eabddf2eab277497463f
prasaddk056 <95524653+prasaddk056@users.noreply.github.com>
2024-03-18 10:25:55 +0530

    Add files via upload

---
+++ Disco Light Effect/script.js
@@ -0,0 +1,29 @@
+const lights = document.querySelectorAll( '.light' );
+const body = document.body;
+
+let active = 0;
+
+setInterval(() => {
+    changeColor();
+} , 1000);
+
+function changeColor(){
+    
+    if(active > 0) {
+        // Turn off the previous light
+        lights[active - 1].classList.remove('active');
+    }
+
+    body.style.backgroundColor = getComputedStyle(lights[active]).getPropertyValue('--light-color');
+
+    lights[active].classList.add('active');
+
+    if(active === lights.length - 1) {
+        setTimeout(()=> {
+            lights[active].classList.remove('active');
+            active = 0;
+        } , 900);
+    } else {
+        active++;
+    }
+}
