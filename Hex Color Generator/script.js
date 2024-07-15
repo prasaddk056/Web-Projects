26164891c03b5af6e422d0424c3a238234ccef2d
prasaddk056 <95524653+prasaddk056@users.noreply.github.com>
2024-07-15 15:48:32 +0530

    Add files via upload

---
+++ Hex Color Generator/script.js
@@ -0,0 +1,31 @@
+
+const generateBtn = document.getElementById("generateBtn");
+
+generateBtn.addEventListener("click",updateColor);
+
+function getRandomColor(){
+
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+
+    for(let i = 0 ; i < 6 ; i++){
+
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
+
+function updateColor(){
+
+    const colorBox = document.getElementById("colorBox");
+    const colorText = document.getElementById("colorText");
+
+    const randomColor = getRandomColor();
+
+    colorBox.style.backgroundColor = randomColor;
+    colorText.textContent = randomColor;
+    document.body.style.backgroundColor = randomColor;
+
+}
+
